2d7f591896eca6405f32e18805ccdbb1675215c6
Andrey Kulagin <ankulskull@gmail.com>
2024-12-04 23:05:39 +0300

    Implemented config

--- cmd/daily/main.go
+++ cmd/daily/main.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/ANkulagin/golang_markdown_converter_sb/internal/config"
+	"log"
+)
+
+func main() {
+	configPath := flag.String("config", "configs/config.yaml", "Путь к конфигурационному файлу")
+	flag.Parse()
+	cfg, err := config.LoadConfig(*configPath)
+	if err != nil {
+		log.Fatalf("Не удалось загрузить конфигурацию: %v", err)
+	}
+	fmt.Printf("src_dir: %s\n", cfg.SrcDir)
+	fmt.Printf("dest_dir: %s\n", cfg.DestDir)
+	fmt.Printf("log_level: %s\n", cfg.LogLevel)
+}
